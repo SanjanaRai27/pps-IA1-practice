da1f7bd041a5d98bab9f24d857c4969858e9142d
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-02-03 10:34:00 +0000

    ..

---
+++ p2final.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input()
+{
+   int a;
+   printf("Enter a number/n");
+   scanf("%d",&a);
+   return a;
+}
+int cmp(int a,int b,int c)
+{
+   if((a>b)&&(a>c))
+   return a;
+   else if(b>c)
+   return b;
+   else
+   return c;
+}
+void output(int largest)
+{
+   printf("%d is the largest number",largest);
+}
+int main()
+{
+   int a,b,c,result;
+   a=input();
+   b=input();
+   c=input();
+   result=cmp(a,b,c);
+   output(result);
+   return 0;
+}
--- p5original.c
+++ p5original.c
@@ -1,31 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 float input()
 {
-  float n;
-  printf("Enter number to find square root\n");
-  scanf("%f",&n);
-  return n;
+    float n;
+    printf("enter the number to find its root:\n");
+    scanf("%f",&n);
+    return n;
 }
-float my_sqrt(float n)
+float my_sqrt(float n) 
+{ 
+    float a=n;  
+    float b;
+    float q=0.000001; 
+    while (fabs(a-b)>q)
+    { 
+        a=(a+b)/2; 
+        b=n/a; 
+    } 
+    return a; 
+} 
+void output(float n,float sqrt_result)
 {
-  float temp,sqr;
-  sqr=n/2;
-  temp=0;
-  while(sqr!=temp)
-  {
-    temp=sqr;
-    sqr=(n/temp+temp)/2;
-  }
-  return sqr;
-}
-void output(float n, float sqrt_result)
-{
-  printf("Square of %f and %f\n",n,sqrt_result);
+    printf("the square of %f is: %f\n",n,sqrt_result);
 }
 int main()
 {
-  float a,s;
-  a=input();
-  s=my_sqrt(a);
-  output(a,s);
+    float a,b;
+    a=input();
+    b=my_sqrt(a);
+    output(a,b);
+    return 0;
 }
